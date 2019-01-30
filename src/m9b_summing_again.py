c8e1d79cee2ed755bea9a93dd38025b72391f529
popoffib <popoffib@rose-hulman.edu>
2019-01-30 18:41:11 -0500

    done with todo 4 and 5

--- src/m9b_summing_again.py
+++ src/m9b_summing_again.py
@@ -79,7 +79,7 @@ def sum_powers(n, p):
 def run_test_sum_powers_in_range():
     """ Tests the   sum_powers_in_range   function. """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement this function.
+    # DONE: 4. Implement this function.
     #   It TESTS the  sum_powers_in_range  function defined below.
     #   Include at least **   3   ** tests.
     #
@@ -91,8 +91,33 @@ def run_test_sum_powers_in_range():
     print('Testing the   sum_powers_in_range   function:')
     print('--------------------------------------------------')
 
+    # Test 1:
+    expected = 142.384
+    answer = sum_powers_in_range(3, 100, 0.1)
+    print('Test 1 expected: ', expected)
+    print('       actual: ', answer)
+
+    # Test 2:
+    expected = 87.143
+    answer = sum_powers_in_range(5, 50, 0.2)
+    print('Test 2 expected: ', expected)
+    print('       actual: ', answer)
+
+    # Test 3:
+    expected = 57.519
+    answer = sum_powers_in_range(4, 20, 0.5)
+    print('Test 3 expected: ', expected)
+    print('       actual: ', answer)
+
 
 def sum_powers_in_range(m, n, p):
+    total = 0
+    for k in range(m, (n+1), 1):
+        if n >= m:
+            n = -n
+        total = total + (k**p)
+    return total
+
     """
     What comes in:  Non-negative integers m and n, with n >= m,
                     and a number p.
@@ -105,7 +130,7 @@ def sum_powers_in_range(m, n, p):
       -- sum_powers_in_range(3, 100, 0.1) returns about 142.384776
     """
     # -------------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #
     #   No fair running the code of  sum_powers_in_range  to GENERATE
